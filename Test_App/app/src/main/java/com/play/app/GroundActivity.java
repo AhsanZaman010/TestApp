29e82e3c64671aabd5c874eeca963e06e6ce514a
Ahsan Zaman <ahsanuz.zaman@accolite.com>
2016-01-15 19:18:41 +0530

    First Push

---
+++ Test_App/app/src/main/java/com/play/app/GroundActivity.java
@@ -0,0 +1,15 @@
+package com.play.app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.accolite_.test_app.R;
+
+public class GroundActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ground);
+    }
+}
